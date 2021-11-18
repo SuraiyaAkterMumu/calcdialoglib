02276b43ae4e4f7809ce633366a3c228af3f6263
Suraiya Akter Mumu <suraiyaaktermumu26@gmail.com>
2021-11-18 09:39:59 +0600

    NewDocumentFileAdd

---
+++ NewDocument.java
@@ -0,0 +1,13 @@
+public class newclass{
+    private float amount;
+ 
+public void setAmount(float amount) {
+    this.amount = amount;
+}
+ 
+public float getAmount() {
+    return this.amount;
+}
+}
+
+newclass obj = new newclass();
